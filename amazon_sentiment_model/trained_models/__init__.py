a7d28a134a18b660db20a67bfe1cea4d4a8aea5d
radhika-minion02 <117430633+radhika-minion02@users.noreply.github.com>
2023-08-21 12:09:10 +0000

    adding trained tokenizer and model

--- amazon_sentiment_model/trained_models/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
-
-from amazon_sentiment_model.config.core import PACKAGE_ROOT, config
-with open(PACKAGE_ROOT / "VERSION") as version_file:
-    __version__ = version_file.read().strip()
